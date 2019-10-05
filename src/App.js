5f5c9ced07dbde9d07936b1e02d30209b4a7d672
Tu <tuvo1106@gmail.com>
2019-10-05 16:30:16 -0700

    Refactor homepage into smaller components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import './App.css'
-import HomePage from './homepage.component'
+import HomePage from './pages/homepage.component'
 
 function App() {
   return (
     <div>
-      Hello world!
       <HomePage></HomePage>)
     </div>
   )
