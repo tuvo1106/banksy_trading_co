f4f2b2a9b6b0483e02007cc22ccc67b57bce608c
Tu <tuvo1106@gmail.com>
2019-10-06 15:32:33 -0700

    Add custom button component and add to sign-in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
 
 import './sign-in.styles.scss'
 
@@ -50,7 +51,7 @@ class SignIn extends Component {
             handleChange={this.handleChange}
             required
           ></FormInput>
-          <input type="submit" value="Submit Form"></input>
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     )
