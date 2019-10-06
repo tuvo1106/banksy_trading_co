cd40c5912765cf9ae6dc3c6927a2270cc07fa17e
Tu <tuvo1106@gmail.com>
2019-10-05 18:53:47 -0700

    Add shop view

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+
+import SHOP_DATA from './shop.data.jsx'
+
+class ShopPage extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      collections: SHOP_DATA
+    }
+  }
+  render() {
+    const { collections } = this.state
+    return (
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview
+            key={id}
+            {...otherCollectionProps}
+          ></CollectionPreview>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default ShopPage
