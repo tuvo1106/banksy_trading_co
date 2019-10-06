1d36a3c9565f723c798a08b283305590f5d75d71
Tu <tuvo1106@gmail.com>
2019-10-05 18:53:37 -0700

    Restructure views

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Directory from '../components/directory/directory.component'
+import Directory from '../../components/directory/directory.component'
 
 import './homepage.styles.scss'
 
