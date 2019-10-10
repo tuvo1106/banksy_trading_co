23cef6e980fcdf8da126232be5b2bd43baf32054
Tu Vo <tuvo1106@gmail.com>
2019-10-10 16:58:57 -0700

    Add sign-in functionality with forms

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import FormInput from '../form-input/form-input.component'
 import CustomButton from '../custom-button/custom-button.component'
 
-import { signInWithGoogle } from './../../firebase/firebase.utils'
+import { auth, signInWithGoogle } from './../../firebase/firebase.utils'
 
 import './sign-in.styles.scss'
 
@@ -17,13 +17,17 @@ class SignIn extends Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault()
 
-    this.setState({
-      email: '',
-      password: ''
-    })
+    const {email, password} = this.state
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password)
+      this.setState({email: '', password: ''})
+    } catch (error) {
+      console.log(error)
+    }
   }
 
   handleChange = event => {
