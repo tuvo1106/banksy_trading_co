b0aa98e0e52601ade55a9e6932218ba57d226e70
Tu <tuvo1106@gmail.com>
2019-10-09 18:20:45 -0700

    Add loggedIn state to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import './App.css'
@@ -6,18 +6,43 @@ import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
+import { auth } from './firebase/firebase.utils'
 
-function App() {
-  return (
-    <div>
-      <Header></Header>
-      <Switch>
-        <Route exact path="/" component={HomePage}></Route>
-        <Route path="/shop" component={ShopPage}></Route>
-        <Route path="/signin" component={SignInAndSignUp}></Route>
-      </Switch>
-    </div>
-  )
+class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    // listen for changes on Firebase
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+    })
+  }
+
+  // close connection
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route path="/shop" component={ShopPage}></Route>
+          <Route path="/signin" component={SignInAndSignUp}></Route>
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
