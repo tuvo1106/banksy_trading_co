5aac901a8091c189094f078ec1dd230687fdbb9f
Tu Vo <tuvo1106@gmail.com>
2019-10-10 11:36:42 -0700

    Add sign-up component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import FormInput from '../form-input/form-input.component'
+import CustomButton from '../custom-button/custom-button.component'
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils'
+
+import './sign-up/styles.scss'
