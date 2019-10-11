74ac53cb2fc9d867ccdb9334cdb692ad6b8c1f82
Tu Vo <tuvo1106@gmail.com>
2019-10-11 10:28:34 -0700

    Abstract api keys to separate file

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,16 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-const config = {
-  apiKey: 'AIzaSyA_53ikiYu-KJy5UUqluGdvKsrwAKCDiv4',
-  authDomain: 'banksy-trading-co.firebaseapp.com',
-  databaseURL: 'https://banksy-trading-co.firebaseio.com',
-  projectId: 'banksy-trading-co',
-  storageBucket: 'banksy-trading-co.appspot.com',
-  messagingSenderId: '705074590890',
-  appId: '1:705074590890:web:169548df66e4838cca8cd3',
-  measurementId: 'G-ERCQ4GS36D'
-}
+import { config } from './firebase.env'
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   // user logged out
@@ -19,7 +10,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const userRef = firestore.doc(`users/${userAuth.uid}`)
   const snapShot = await userRef.get()
   if (!snapShot.exists) {
-    const {displayName, email} = userAuth
+    const { displayName, email } = userAuth
     const createdAt = new Date()
 
     // create new user
