a31c0ca4c524e794e752aef6cf9f1f42f1323e96
Tu Vo <tuvo1106@gmail.com>
2019-10-11 09:51:58 -0700

    Update styling on collection-items

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
+import CustomButton from '../custom-button/custom-button.component'
 import './collection-item.styles.scss'
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className="collection-item">
+  <div className='collection-item'>
     <div
-      className="image"
-      style={{ backgroundImage: `url(${imageUrl})` }}
-    ></div>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+      className='image'
+      style={{ backgroundImage: `url(${imageUrl})` }}></div>
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
+    <CustomButton inverted>Add to cart</CustomButton>
   </div>
 )
 
--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,11 +2,17 @@ import React from 'react'
 
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
-    {...otherProps}
-  >
+    className={`${inverted ? 'inverted' : ''} ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
+    {...otherProps}>
     {children}
   </button>
 )
