f419792990622cf7bd2ef5858dfe3b9e74b05476
Tu Vo <tuvo1106@gmail.com>
2019-10-11 18:04:45 -0700

    Allow users to modify items in cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import { CartActionTypes } from './cart.types'
-import { addItemToCart } from './cart.utils'
+import { addItemToCart, removeItemFromCart } from './cart.utils'
 
 const INITIAL_STATE = {
   hidden: true,
@@ -24,6 +24,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         cartItems: state.cartItems.filter(
           cartItem => cartItem.id !== action.payload.id
         )
+      }
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
       }
     default:
       return state
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,6 +9,22 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
         : cartItem
     )
   }
-
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
+}
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? {
+        ...cartItem,
+        quantity: cartItem.quantity - 1
+      }
+      : cartItem
+  )
 }
