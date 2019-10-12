e87d426f3f0b08942e293b71458d9be10b5bf5ae
Tu Vo <tuvo1106@gmail.com>
2019-10-11 17:22:26 -0700

    Add dispatch shorthand to toggle cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,10 +6,11 @@ import { withRouter } from 'react-router-dom'
 import CustomButton from './../custom-button/custom-button.component'
 import CartItem from '../cart-item/cart-item.component'
 import { selectCartItems } from '../../redux/cart/cart.selectors'
+import { toggleCartHidden } from '../../redux/cart/cart.actions'
 
 import './cart-dropdown.scss'
 
-const CartDropdown = ({ cartItems, history }) => (
+const CartDropdown = ({ cartItems, history, dispatch }) => (
   <div className='cart-dropdown'>
     <div className='cart-items'>
       {cartItems.length ? (
@@ -20,7 +21,11 @@ const CartDropdown = ({ cartItems, history }) => (
         <span className='empty-message'>Your cart is empty</span>
       )}
     </div>
-    <CustomButton onClick={() => history.push('./checkout')}>
+    <CustomButton
+      onClick={() => {
+        history.push('./checkout')
+        dispatch(toggleCartHidden())
+      }}>
       Checkout
     </CustomButton>
   </div>
