211e38f7255b838ac8a337cc6f39f1dd104faaff
Tu Vo <tuvo1106@gmail.com>
2019-10-11 17:35:05 -0700

    Add checkout-item component

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,8 @@ import {
   selectCartTotal
 } from '../../redux/cart/cart.selectors'
 
+import CheckoutItem from '../../components/checkout-item/checkout-item'
+
 import './checkout.styles.scss'
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -28,7 +30,9 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(cartItem => cartItem.name)}
+    {cartItems.map(cartItem => (
+      <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+    ))}
     <div className='total'>
       <span>Total: ${total}</span>
     </div>
