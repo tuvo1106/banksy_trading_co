00e0f98187968a101ca47015434fab643b1791ee
Tu <tuvo1106@gmail.com>
2019-10-14 13:54:34 -0700

    Add redux persist to persist changes to cart

--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './index.css'
 import App from './App'
 
-import store from './redux/store'
+import { store, persistor } from './redux/store'
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
+import { persistStore } from 'redux-persist'
 
 import rootReducer from './root-reducer'
 
 const middlewares = [logger]
-
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
+const persistor = persistStore(store)
 
-export default store
+export { store, persistor }
