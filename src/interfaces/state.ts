7e7045cd2858b0104aa4274b2e1ceeaf9892ec3d
Tu Vo <tuvo1106@gmail.com>
2019-10-15 17:00:55 -0700

    Add user and state interfaces

---
+++ src/interfaces/state.ts
@@ -0,0 +1,7 @@
+export interface state {
+  cart?: any
+  directory?: any
+  shop?: any
+  user?: any
+  _persist?: any
+}
---
+++ src/interfaces/user.ts
@@ -0,0 +1,6 @@
+export interface user {
+  createdAt?: any
+  displayName: string
+  email: string
+  id: string
+}
