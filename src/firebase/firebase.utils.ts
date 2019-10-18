63bb6a871e97c55dfcb3ee8ca4d31abb9ca2c60e
Tu Vo <tuvo1106@gmail.com>
2019-10-18 01:01:39 -0700

    Change variable names in firebase

--- src/firebase/firebase.utils.ts
+++ src/firebase/firebase.utils.ts
@@ -70,8 +70,8 @@ export const convertCollectionsSnapshotToMap = (collections: any) => {
 export const auth = firebase.auth()
 export const firestore = firebase.firestore()
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({ prompt: "select_account" })
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+googleProvider.setCustomParameters({ prompt: "select_account" })
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
 
 export default firebase
