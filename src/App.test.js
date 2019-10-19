fed715610792b60e1455c3cc7bc321c3673d07ca
Tu Vo <tuvo1106@gmail.com>
2019-10-18 23:16:50 -0700

    Set up initial tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,23 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
-it("renders without crashing", () => {
-  const div = document.createElement("div")
-  ReactDOM.render(<App />, div)
+import { store, persistor } from './redux/store'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>,
+    div
+  )
   ReactDOM.unmountComponentAtNode(div)
 })
