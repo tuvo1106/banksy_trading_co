861d00eef06bfe34d9f58c0e0a316713ed42f413
Tu Vo <tuvo1106@gmail.com>
2019-10-18 19:52:27 -0700

    Add media queries to make app more mobile friendly

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from "react-router-dom"
 import { connect } from "react-redux"
 import { createStructuredSelector } from "reselect"
 
-import "./App.css"
+import "./App.scss"
 import HomePage from "./pages/homepage/homepage.component"
 import ShopPage from "./pages/shop/shop.component"
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"
