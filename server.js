98250eaf8b27df35a7daf2e784cb87115889a173
Tu Vo <tuvo1106@gmail.com>
2019-10-20 21:27:32 -0700

    Connect stripe front end to back end

--- server.js
+++ server.js
@@ -7,11 +7,14 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
 }
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true }))
+
 app.use(cors())
 
 if (process.env.NODE_ENV === "production") {
@@ -23,5 +26,20 @@ if (process.env.NODE_ENV === "production") {
 
 app.listen(port, error => {
   if (error) throw error
-  console.log("Server running on port" + port)
+  console.log("Server running on port " + port)
+})
+
+app.post("/payment", (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: "usd"
+  }
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr })
+    } else {
+      res.status(200).send({ success: stripeRes })
+    }
+  })
 })
