4998ddb09e541ec4a9cf4c5e4e4df381dff9e4b3
Tu Vo <tuvo1106@gmail.com>
2019-10-25 13:23:20 -0700

    Add gzip compression module to server

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParse = require('body-parser')
-const path = require('path')
+const express = require("express")
+const cors = require("cors")
+const bodyParse = require("body-parser")
+const path = require("path")
+const compression = require("compression")
 
 // New Code
-const expressLayouts = require('express-ejs-layouts')
-const mongoose = require('mongoose')
-const passport = require('passport')
-const flash = require('connect-flash')
-const session = require('express-session')
+const expressLayouts = require("express-ejs-layouts")
+const mongoose = require("mongoose")
+const passport = require("passport")
+const flash = require("connect-flash")
+const session = require("express-session")
 // end
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
 }
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 const app = express()
 const port = process.env.PORT || 5000
 
+app.use(compression())
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true }))
 
@@ -28,10 +30,10 @@ app.use(cors())
 // Body of new code
 
 // Passport Config
-require('./config/passport')(passport)
+require("./config/passport")(passport)
 
 // DB Config
-const db = require('./config/keys').mongoURI
+const db = require("./config/keys").mongoURI
 
 // Connect to MongoDB
 mongoose
@@ -39,17 +41,17 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(() => console.log('MongoDB Connected'))
+  .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err))
 
 // EJS
 app.use(expressLayouts)
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs")
 
 // Express session
 app.use(
   session({
-    secret: 'secret',
+    secret: "secret",
     resave: true,
     saveUninitialized: true
   })
@@ -63,16 +65,16 @@ app.use(passport.session())
 app.use(flash())
 
 // Global variables
-app.use(function(req, res, next) {
-  res.locals.success_msg = req.flash('success_msg')
-  res.locals.error_msg = req.flash('error_msg')
-  res.locals.error = req.flash('error')
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg")
+  res.locals.error_msg = req.flash("error_msg")
+  res.locals.error = req.flash("error")
   next()
 })
 
 // Routes
-app.use('/', require('./routes/index.js'))
-app.use('/users', require('./routes/users.js'))
+app.use("/", require("./routes/index.js"))
+app.use("/users", require("./routes/users.js"))
 
 // end of Body
 
@@ -83,21 +85,21 @@ app.use('/users', require('./routes/users.js'))
   })
 } */
 
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+app.use(express.static(path.join(__dirname, "client/build")))
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"))
 })
 
 app.listen(port, error => {
   if (error) throw error
-  console.log('Server running on port ' + port)
+  console.log("Server running on port " + port)
 })
 
-app.post('/payment', (req, res) => {
+app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd'
+    currency: "usd"
   }
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
