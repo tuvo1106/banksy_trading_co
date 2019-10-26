fab09d6cd254385276c4cb62b7563e3bb234176f
Farrukh <farruh1996@gmail.com>
2019-10-26 11:07:55 -0700

    Remove ejs templates

--- server.js
+++ server.js
@@ -22,22 +22,15 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(compression())
-
 // Force HTTPS
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }))
 }
 
+app.use(compression())
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true }))
-
 app.use(cors())
-
-// Body of new code
-
-// Passport Config
-require("./config/passport")(passport)
 
 // DB Config
 const db = require("./config/keys").mongoURI
@@ -51,46 +44,8 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err))
 
-// EJS
-app.use(expressLayouts)
-app.set("view engine", "ejs")
-
-// Express session
-app.use(
-  session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true
-  })
-)
-
-// Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
-
-// Connect flash
-app.use(flash())
-
-// Global variables
-app.use((req, res, next) => {
-  res.locals.success_msg = req.flash("success_msg")
-  res.locals.error_msg = req.flash("error_msg")
-  res.locals.error = req.flash("error")
-  next()
-})
-
 // Routes
-app.use("/", require("./routes/index.js"))
 app.use("/users", require("./routes/users.js"))
-
-// end of Body
-
-/* if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")))
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"))
-  })
-} */
 
 app.use(express.static(path.join(__dirname, "client/build")))
 app.get("*", (req, res) => {
