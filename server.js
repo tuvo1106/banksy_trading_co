f7c6a069cf92a0f2713e33d860a8063dc0363764
Farrukh <farruh1996@gmail.com>
2019-10-26 11:31:25 -0700

    revert back changes

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const bodyParse = require("body-parser")
 const path = require("path")
 const compression = require("compression")
 const enforce = require('express-sslify')
+
+// New Code
+const expressLayouts = require("express-ejs-layouts")
 const mongoose = require("mongoose")
+const passport = require("passport")
+const flash = require("connect-flash")
+const session = require("express-session")
+// end
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config()
@@ -15,15 +22,21 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const app = express()
 const port = process.env.PORT || 5000
 
-// Force HTTPS
+app.use(compression())
+
 if (process.env.NODE_ENV === "production") {
   app.use(enforce.HTTPS({ trustProtoHeader: true }))
 }
 
-app.use(compression())
 app.use(bodyParse.json())
 app.use(bodyParse.urlencoded({ extended: true }))
+
 app.use(cors())
+
+// Body of new code
+
+// Passport Config
+require("./config/passport")(passport)
 
 // DB Config
 const db = require("./config/keys").mongoURI
@@ -37,8 +50,46 @@ mongoose
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err))
 
+// EJS
+app.use(expressLayouts)
+app.set("view engine", "ejs")
+
+// Express session
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+)
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+// Connect flash
+app.use(flash())
+
+// Global variables
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg")
+  res.locals.error_msg = req.flash("error_msg")
+  res.locals.error = req.flash("error")
+  next()
+})
+
 // Routes
+app.use("/", require("./routes/index.js"))
 app.use("/users", require("./routes/users.js"))
+
+// end of Body
+
+/* if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")))
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"))
+  })
+} */
 
 app.use(express.static(path.join(__dirname, "client/build")))
 app.get("*", (req, res) => {
