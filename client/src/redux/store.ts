d0a4938fa4c88d0e0f441f3a9bb0424b0fa6abc2
Farrukh <farruh1996@gmail.com>
2019-10-31 12:48:01 -0700

    remove redux logger

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -9,11 +9,10 @@ import rootSaga from './root-saga'
 const sagaMiddleware = createSagaMiddleware()
 const middlewares: any[] = [sagaMiddleware]
 
-// if (process.env.NODE_ENV === "development") {
-//   middlewares.push(logger)
-// }
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger)
+}
 
-middlewares.push(logger)
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
 
 sagaMiddleware.run(rootSaga)
