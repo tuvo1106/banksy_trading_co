b123ecacc5e87eea264af24d230547d9c9466e10
Tu <tuvo1106@gmail.com>
2019-11-06 11:12:15 -0800

    Add documentation and comments

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,34 @@
 /* modules */
-import React, { useEffect, lazy, Suspense } from "react"
-import { Switch, Route, Redirect } from "react-router-dom"
-import { connect } from "react-redux"
-import { createStructuredSelector } from "reselect"
+import React, { useEffect, lazy, Suspense } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { createStructuredSelector } from 'reselect'
 
 /* components */
-import Header from "./components/header/header.component"
-import Footer from "./components/footer/footer.component"
-import Spinner from "./components/spinner/spinner.component"
-import ErrorBoundary from "./components/error-boundary/error-boundary.component"
+import Header from './components/header/header.component'
+import Footer from './components/footer/footer.component'
+import Spinner from './components/spinner/spinner.component'
+import ErrorBoundary from './components/error-boundary/error-boundary.component'
 
 /* actions */
-import { checkUserSession } from "./redux/user/user.actions"
+import { checkUserSession } from './redux/user/user.actions'
 
 /* selectors */
-import { selectCurrentUser } from "./redux/user/user.selector"
+import { selectCurrentUser } from './redux/user/user.selector'
 
 /* interfaces */
-import { user } from "./interfaces/user"
+import { user } from './interfaces/user'
 
 /* styles */
-import "./App.scss"
+import './App.scss'
 
-const HomePage = lazy(() => import("./pages/homepage/homepage.component"))
-const ShopPage = lazy(() => import("./pages/shop/shop.component"))
+/* lazy loading for better performance, splits code into smaller chunks */
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'))
+const ShopPage = lazy(() => import('./pages/shop/shop.component'))
 const SignInAndSignUp = lazy(() =>
-  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+  import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component')
 )
-const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"))
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'))
 
 interface AppProps {
   checkUserSession: Function
@@ -38,21 +39,22 @@ const App = ({ checkUserSession, currentUser }: AppProps) => {
   useEffect(() => {
     checkUserSession()
   }, [checkUserSession])
-
   return (
     <div>
       <Header />
       <Switch>
         <ErrorBoundary>
+          {/* HOC catches all errors in application */}
           <Suspense fallback={<Spinner />}>
-            <Route exact path='/' component={HomePage} />
-            <Route path='/shop' component={ShopPage} />
-            <Route exact path='/checkout' component={CheckoutPage}></Route>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage}></Route>
             <Route
               exact
-              path='/signin'
+              path="/signin"
               render={() =>
-                currentUser ? <Redirect to='/' /> : <SignInAndSignUp />
+                /* if user is logged in, redirect to home */
+                currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
               }
             />
           </Suspense>
